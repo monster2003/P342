3c9ab82c1a77e49c0ef3660f73f5d1a71eb81562
monster2003 <v.alex.4mail@gmail.com>
2016-10-30 13:54:46 +0300

    compiet

--- P342.pas
+++ P342.pas
@@ -4,5 +4,14 @@
  * Date: 30.10.2016
  *)
 program P342;
+var
+    sum,i,a:Integer ;
 begin
+    sum:=0;
+    for i:=1 to 100 do
+    begin
+        ReadLn(a);
+        sum:=sum+a;
+    end;
+    WriteLn(sum);
 end.
